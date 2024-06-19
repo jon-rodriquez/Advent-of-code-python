dff12a4076e1e3c1e17f41aaa097c1103c88d927
Jonathan Rodriquez <jrodriquez.07.02@gmail.com>
2024-06-19 15:52:23 -0400

    part2 of day 1

--- day-1/Trebuchet.py
+++ day-1/Trebuchet.py
@@ -1,15 +1,16 @@
-# read file line by line
-
-
 # deterine all numbers in a line of text
 def getNumbers(s):
     number = []
 
-    for charater in s:
+    for i, charater in enumerate(s):
         if charater.isdigit():
             number.append(charater)
+
+        for j, num in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
+            if s[i:].startswith(num):
+                number.append(str(j+1))
     
-    num = number[0] + number[len(number)-1]
+    num = number[0] + number[-1]
     return int(num)
 
 
@@ -21,10 +22,9 @@ def readFile(filename):
     for line in file:
         sum += getNumbers(line)
 
-
-
     file.close()
     print(sum)
 
 
 readFile("./input.txt")
+
